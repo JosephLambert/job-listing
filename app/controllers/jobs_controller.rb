70a974f845c408d1c75df1274f526a43142726df
josephlambert <478911359@qq.com>
2017-04-25 10:28:13 +0800

    add search function

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :validate_search_key, only: [:search]
     def index
         @jobs = case params[:order]
                 when 'by_lower_bound'
@@ -51,6 +52,24 @@ class JobsController < ApplicationController
         @job = Job.find(params[:id])
         @job.destroy
         redirect_to jobs_path, alert: 'Job deleted '
+    end
+
+    def search
+        if @query_string.present?
+            search_result = Job.published.ransack(@search_criteria).result(distinct: true)
+            @jobs = search_result.recent.paginate(page: params[:page], per_page: 5)
+      end
+    end
+
+    protected
+
+    def validate_search_key
+        @query_string = params[:q].gsub(/\\|\'|\/|\?/, '') if params[:q].present?
+        @search_criteria = search_criteria(@query_string)
+    end
+
+    def search_criteria(query_string)
+        { title_or_location_or_company_cont: query_string }
     end
 
     private
