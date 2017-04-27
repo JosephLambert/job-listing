63df440b2823c678efab03b22d924a2c3965b733
josephlambert <478911359@qq.com>
2017-04-27 10:57:18 +0800

    validates format done

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,6 @@
 class Feedback < ApplicationRecord
+    validates :name, presence: { message: '请填写姓名' }
+    validates :email, presence: { message: '请填写邮箱' }
+    validates :comment, presence: { message: '请填写问题或者建议' }
+    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: '请输入正确的邮箱格式'
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -7,10 +7,17 @@ class Job < ApplicationRecord
     has_many :fav_relationships
     has_many :favors, through: :fav_relationships, source: :user
 
-    validates :title, presence: true
-    validates :wage_upper_bound, presence: true
-    validates :wage_lower_bound, presence: true
-    validates :wage_lower_bound, numericality: { greater_than: 0 }
+    validates :title, presence: { message: '请填写职称' }
+    validates :description, presence: { message: '请填写职位描述' }
+    validates :wage_upper_bound, presence: { message: '请填写薪资上限' }
+    validates :wage_lower_bound, presence: { message: '请填写薪资下限' }
+    validates :wage_lower_bound, numericality: { greater_than: 0, message: '薪资下限必须大于零' }
+    validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound, message: '薪资上限必须大于薪资下限' }
+    validates :contact_email, presence: { message: '请填写联系邮箱' }
+    validates_format_of :contact_email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: '请输入正确的邮箱格式'
+    validates :company, presence: { message: '请填写公司名称' }
+    validates :location, presence: { message: '请填写公司所在城市' }
+    validates :category, presence: { message: '请填写职位类别' }
 
     def publish!
         self.is_hidden = false
